9b2f8be04f8e33c94915aeb4d8b8fe3fddd75fed
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-02-18 08:16:56 +0100

    feat(RecordReport): Added stub for ReportRecord.
    Naming ReportRecord or RecordReport should change.

--- Reporting/RecordReport.swift
+++ Reporting/RecordReport.swift
@@ -2,12 +2,24 @@ import SwiftUI
 import TPPDF
 
 struct RecordReport: Report {
-     func generateDocument() -> PDFDocument {
+    let reportRecord: ReportRecord
+    
+     func generateDocument() -> [PDFDocument] {
          let document = PDFDocument(format: .a4)
-         document.add(.contentCenter, text: "Stub RecordReport")
+         document.add(.contentCenter, text: "RecordReport")
          document.add(space: 10.0)
          let style = PDFLineStyle(type: .full, color: .darkGray, width: 0.5)
          document.addLineSeparator(PDFContainer.contentLeft, style: style)
-         return document
+
+         document.add(space: 10.0)
+         document.add(.contentLeft, text: reportRecord.text)
+         let amountFormatted = reportRecord.amount.formatted(
+            .number
+                .grouping(.automatic)
+                .precision(.fractionLength(2...2))
+         )
+         document.add(.contentCenter, text: amountFormatted)
+         
+         return [document]
      }
 }

aa26fdc320d1fdf6936127a88da806dec58a710b
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-02-21 17:07:12 +0100

    feat: Added Trafina Logo to SingleBookingReport

--- Reporting/SingleBookingReport.swift
+++ Reporting/SingleBookingReport.swift
@@ -6,26 +6,35 @@ import AppKit
 import TPPDF
 import ImageCompressionKit
 import Extensions
-
+import CoreGraphics
 
 struct SingleBookingReport: Report {
     let reportRecord: ReportRecord
     
     private let style = PDFLineStyle(type: .full, color: .darkGray, width: 0.5)
+    private let logoSize = CGSize(width: 200, height: 70)
+    
+    
     private var logo: PDFImage {
-        let size = CGSize(width: 70,
-                          height: 70
-        )
-        guard let resizedImage = logoImage.resized(to: size),
+        guard let resizedImage = logoImage.resized(to: logoSize),
               let finalImage = resizedImage.replacingTransparentPixels(with: .white)
         else { fatalError() }
         return PDFImage(image: finalImage)
     }
+
+    
     func generateDocument() -> [PDFDocument] {
         let document = PDFDocument(format: .a4)
-        
-        
-        document.add(.headerRight, image: logo)
+       
+        // not really a header is in the content area....
+        let headerGroup = PDFGroup(allowsBreaks: false,
+                                   backgroundColor: .green,
+                                   padding: EdgeInsets(top: 2, left: 2, bottom: 2, right: 2 )
+                                   )
+        headerGroup.add(.right, image: logo)
+        document.add(group: headerGroup)
+        document.add(space: 20.0)
+//        document.add(.headerRight, image: PDFImage(image: logoImage, size: logoSize, options: .rounded))
         
         document.addLineSeparator(PDFContainer.contentLeft, style: style)
         document.add(space: 10.0)

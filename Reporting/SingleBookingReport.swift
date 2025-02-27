88dc062a12eedda3f6dd56bcb4e9d821e6f47ab5
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-02-27 22:19:52 +0100

    feat: SingleBookingReport - printing all booking info except scans
    Added ReportRecord fields to report.
    Scans missing.

--- Reporting/SingleBookingReport.swift
+++ Reporting/SingleBookingReport.swift
@@ -11,19 +11,26 @@ import CoreGraphics
 struct SingleBookingReport: Report {
     let reportRecord: ReportRecord
     
-    private let style = PDFLineStyle(type: .full, color: .darkGray, width: 0.5)
-    private let logoSize = CGSize(width: 100, height: 50)
-    
+    private let dividerLineStyle = PDFLineStyle(type: .full, color: .darkGray, width: 0.5)
+    private let logoSize = CGSize(width: 100, height: 70)
+    private let digitStyle = PDFTableCellStyle(font: SingleBookingFonts.digit)
+    private let boldTextStyle = PDFTableCellStyle(font: SingleBookingFonts.bold)
+    private let textStyle = PDFTableCellStyle(font: SingleBookingFonts.regular)
+
+    private let digitTableStyle = PDFTableCellStyle(
+        borders: PDFTableCellBorders(top: PDFLineStyle(type: .full, color: .darkGray, width: 0.5)),
+        font: SingleBookingFonts.digit
+    )
     
     private var logo: PDFImage {
         guard let resizedImage = logoImage.resized(to: logoSize, alignment: .right)
         else { fatalError() }
         let finalImage = resizedImage.fillFrame(frameColor: .white).addFrame(frameColor: .lightGray)
-        return PDFImage(image: finalImage)
+        return PDFImage(image: finalImage, quality: 1.0)
     }
-    private var cashFlowFormatted: PDFSimpleText {
-        PDFSimpleText(text: reportRecord.cashFlow)
-  }
+    private var cashFlowFormatted: String {
+            reportRecord.cashFlow
+    }
     
     private var amountFormatted: String { 
         let amountValue = Decimal(reportRecord.amount) // Ensure Decimal type
@@ -34,18 +41,19 @@ struct SingleBookingReport: Report {
         )
     }
     private var dateFormatted: String {
-        return reportRecord.date.formatted(
-            date: .long, 
-            time: .omitted)
+        let style = Date.FormatStyle(date: .long, time: .omitted)
+            .locale(Locale(identifier: "de-CH"))
+        return reportRecord.date.formatted(style)
     }
+
     
-    private var iconImage: PDFImage {
-        let symbolSize = CGSize(width: 20, height: 20)
+    private var iconImage: PlatformImage {
+        let symbolSize = CGSize(width: 40, height: 40)
        let symbolImage = UIImage(systemName: reportRecord.icon) ?? UIImage(systemName: "questionmark")!
         guard let resizedImage = symbolImage.resized(to: symbolSize, alignment: .left)
         else { fatalError() }
         let finalImage = resizedImage.fillFrame(frameColor: .white).addFrame(frameColor: .lightGray)
-        return PDFImage(image: finalImage)
+        return finalImage
     }
     
     func generateDocument() -> [PDFDocument] {
@@ -55,23 +63,37 @@ struct SingleBookingReport: Report {
         document.add(.contentRight, image: logo)
         document.add(space: 20.0)
         
-        document.addLineSeparator(PDFContainer.contentLeft, style: style)
-        document.add(space: 10.0)
-        document.set(font: SingleBookingFonts.bold)
-        document.add(.contentLeft, textObject: cashFlowFormatted)
-        document.set(font: SingleBookingFonts.regular)
-        document.add(.contentRight, text: dateFormatted) 
-        document.add(.contentRight, text: amountFormatted)
-        document.add(.contentLeft, image: iconImage)
-        document.add(.contentLeft, text: reportRecord.text)
-        document.add(space: 10.0)
-        document.addLineSeparator(PDFContainer.contentLeft, style: style)
-        document.add(space: 10.0)
+        document.addLineSeparator(PDFContainer.contentLeft, style: dividerLineStyle)
+        // Add booking information as table
+        let row1Table = PDFTable(rows: 1, columns: 2)
+        row1Table.style = rowTableStyle
+        let row1 = row1Table[row: 0]
+        row1.content = [cashFlowFormatted , dateFormatted]
+        row1.style = [boldTextStyle, textStyle]
+        row1.alignment = [.left, .right]
+        row1Table.margin = 5.0
         
-
+        document.add(table: row1Table)
         
+        let row2Table = PDFTable(rows: 1, columns: 3)
+        row2Table.widths = [0.1, 0.6, 0.3]
+        row2Table.style = rowTableStyle
+        let row2 = row2Table[row: 0]
+        row2.content = [iconImage, reportRecord.text, amountFormatted]
+        row2.style = [textStyle, textStyle, digitStyle]
+        row2.alignment = [.left, .left, .right] 
+        // Set table padding and margin
+        row2Table.margin = 5.0
+        document.add(table: row2Table)
+        //
+        
+        document.add(space: 10.0)
+        document.addLineSeparator(PDFContainer.contentLeft, style: dividerLineStyle)
        
+        // add scans
         
+        //
+
         return [document]
     }
 }
@@ -89,3 +111,22 @@ fileprivate struct SingleBookingFonts {
     static  let digitBold = NSFont.monospacedDigitSystemFont(ofSize: 15, weight: .bold)
 #endif
 }
+fileprivate var rowTableStyle: PDFTableStyle {
+    PDFTableStyle(
+        rowHeaderCount: 0,
+        columnHeaderCount: 0,
+        footerCount: 0,
+        outline: PDFLineStyle(type: .none, color: .white, width: 0),
+        rowHeaderStyle: PDFTableCellStyle(),
+        columnHeaderStyle: PDFTableCellStyle(),
+        footerStyle: PDFTableCellStyle(),
+        contentStyle: PDFTableCellStyle(
+            borders: PDFTableCellBorders(
+                left: PDFLineStyle(type: .none),
+                top: PDFLineStyle(type: .none),
+                right: PDFLineStyle(type: .none),
+                bottom: PDFLineStyle(type: .none)
+            )),
+        alternatingContentStyle: nil
+    )
+    }

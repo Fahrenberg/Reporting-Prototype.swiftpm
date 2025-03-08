7690b045c2a0634c52b5c204a40d8d581fbec85e
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-03-08 20:53:03 +0100

    refactor: ReportRecord - storing Scan Filename as String instead of scan as Data
    Loading scans only when needed.

--- Reporting/ReportRecord.swift
+++ Reporting/ReportRecord.swift
@@ -18,26 +18,29 @@ struct ReportRecord {
     let icon: String
     let text: String
     let amount: Double
-    let scans: [Data]
     var cashFlow: String { amount >=  0 ? "Einzahlung" : "Auszahlung" }
+    let scanFilename: [String]
     
+    var scans: [Data] { 
+        return scanFilename.map { imageData(filename: $0)}
+    }
     static func mock(scanCount: Int = 5) -> ReportRecord {
         ReportRecord(
             date: Date().addingTimeInterval(Double.random(in: -1_000_000_000 ... 0)),
             icon: "truck.box",
-            text: "Mock Report Record",
+            text: "Mock Report Record with \(scanCount) scan(s)",
             amount: Double.random(in: 1000.0 ... 10000.00),
             // Randomly select scanCount scan images
-            scans: Array(allImageData().shuffled().prefix(scanCount)) 
+            scanFilename: Array(allImageFilenames().shuffled().prefix(scanCount))
         )
     }
 }
 
 struct ReportRecords {
-    static func mocks(count: Int = 5) -> [ReportRecord] {
+    static func mocks(count: Int = 6) -> [ReportRecord] {
         var reportRecords: [ReportRecord] = []
-        for i in 0...count {
-            let reportRecord = ReportRecord.mock()
+        for i in 0..<count {
+            let reportRecord = ReportRecord.mock(scanCount: i)
             reportRecords.append( reportRecord )
         }
         return reportRecords

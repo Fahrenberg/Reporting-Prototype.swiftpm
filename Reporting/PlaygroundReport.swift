c8d7cae345e4bf1ed8989ca03b71549345e732b5
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2025-03-10 16:48:18 +0100

    feat: Added red retangle as group

--- Reporting/PlaygroundReport.swift
+++ Reporting/PlaygroundReport.swift
@@ -24,36 +24,59 @@ struct PlaygroundReport: Report {
         )
     }
     
+    private var logo: PDFImage {
+        let logoSize = CGSize(width: 150, height: 47)
+        guard let resizedImage = logoImage.resized(to: logoSize, alignment: .right)
+        else { fatalError() }
+        let finalImage = resizedImage.fillFrame(frameColor: .white).addFrame(frameColor: .lightGray)
+        return PDFImage(image: finalImage, options: [.none])
+    }
     
-    func generateDocument() -> [PDFDocument] {
-        
-        
-        let size = CGSize(width: 100, height: 100)
+    private func redRectangle(size: CGSize) -> PDFGroup {
         let path = PDFBezierPath(ref: CGRect(origin: .zero, size: size))
-        path.move(to: PDFBezierPathVertex(position: CGPoint(x: size.width / 2, y: 0),
-                                          anchor: .topCenter))
-        path.addLine(to: PDFBezierPathVertex(position: CGPoint(x: size.width, y: size.height / 2),
-                                             anchor: .middleRight))
-        path.addLine(to: PDFBezierPathVertex(position: CGPoint(x: size.width / 2, y: size.height),
-                                             anchor: .bottomCenter))
-        path.addLine(to: PDFBezierPathVertex(position: CGPoint(x: 0, y: size.height / 2),
-                                             anchor: .middleLeft))
+        path.move(to: PDFBezierPathVertex(
+            position: CGPoint.zero,
+            anchor: .topLeft)
+        )
+        path.addLine(to: PDFBezierPathVertex(
+            position: CGPoint(x: size.width, y: 0), 
+            anchor: .topLeft)
+        )
+        path.addLine(to: PDFBezierPathVertex(
+            position: CGPoint(x: size.width, y: size.height),
+            anchor: .topLeft)
+        )
+        path.addLine(to: PDFBezierPathVertex(
+            position: CGPoint(x: 0, y: size.height),
+            anchor: .topLeft)
+        )
+        path.addLine(to: PDFBezierPathVertex(
+            position: CGPoint(x: 0, y: 0), 
+            anchor: .topLeft)
+        )
+        
         path.close()
         
-        let shape = PDFDynamicGeometryShape(path: path, fillColor: .white, stroke: .init(type: .full, color: .red, width: 5.0, radius: 0))
-
+        let shape = PDFDynamicGeometryShape(
+            path: path, 
+            fillColor: .clear,
+            stroke: .init(type: .full, color: .green, width: 2.0, radius: 0)
+        )
+        
         // Create the group object and set the background color and shape
         let group = PDFGroup(allowsBreaks: false,
-                             backgroundColor: .lightGray,
-                             backgroundShape: shape,
-                             padding: EdgeInsets(top: 200, left: 200, bottom: 200, right: 200)
-                            )
-        
-        
-        group.add(text: " ddddd ")
+                             backgroundColor: .none,
+                             backgroundShape: shape
+        ) 
+        return group
+    }
+    
+    func generateDocument() -> [PDFDocument] {
+        let group = redRectangle(size: CGSize(width: 200, height: 300))
+        group.add(space: 50)
+        group.set(indentation: 30, left: true)
+        group.add(image: logo)
         document.add(group: group)
-        
-        
         
         return [document]
     }
